d2ee52c6bc5ea0231bf3683295e3fa12f2a52134
gbad1973 <gbad1973@gmail.com>
2026-02-24 13:00:55 -0600

    Agrega logs de depuración en wsgi.py

--- controltarjetas/wsgi.py
+++ controltarjetas/wsgi.py
@@ -8,8 +8,10 @@ https://docs.djangoproject.com/en/5.2/howto/deployment/wsgi/
 """
 
 import os
-
 from django.core.wsgi import get_wsgi_application
+
+
+print("=== WSGI cargado correctamente ===")
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'controltarjetas.settings')
 
