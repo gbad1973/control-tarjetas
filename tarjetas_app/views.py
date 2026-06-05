3cb6c6febd8c56a2cf933b2a560304b13afb0305
gbad1973 <gbad1973@gmail.com>
2026-06-05 14:04:04 -0600

    Agrega consolidado de todas las tarjetas activas en dashboard

--- tarjetas_app/views.py
+++ tarjetas_app/views.py
@@ -334,8 +334,12 @@ def lista_establecimientos(request):
 
 # ========== LISTA MOVIMIENTOS (CON AGRUPACIÓN IGUAL A DETALLE PERSONA) ==========
 
+from django.views.decorators.csrf import csrf_exempt
+from django.views.decorators.http import require_http_methods
 
 @login_required
+@require_http_methods(["GET"])
+
 def lista_movimientos(request):
     from django.db.models import Sum, Q, Prefetch
     from django.core.paginator import Paginator
