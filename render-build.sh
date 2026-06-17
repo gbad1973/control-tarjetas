eda4bbed183bf6439b356500de708c56facc54d9
gbad1973 <gbad1973@gmail.com>
2026-06-17 13:15:01 -0600

    Agrega render-build.sh con migraciones automáticas

---
+++ render-build.sh
@@ -0,0 +1,13 @@
+
+#!/bin/bash
+
+echo "Instalando dependencias..."
+pip install -r requirements.txt
+
+echo "Ejecutando migraciones..."
+python manage.py migrate --noinput
+
+echo "Recolectando archivos estáticos..."
+python manage.py collectstatic --noinput
+
+echo "Build completado con éxito."
